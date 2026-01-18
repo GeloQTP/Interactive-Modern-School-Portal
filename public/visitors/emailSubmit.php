f250309682d88b900bd282e92b7eeea579ee53c9
GeloQTP <geloadrian24@gmail.com>
2026-01-19 02:14:19 +0800

    configure gitignore,add avoid duplicate email subscription logic, configured loginPage, add loader

--- public/visitors/emailSubmit.php
+++ public/visitors/emailSubmit.php
@@ -1,23 +1,32 @@
 <?php
-session_start();
 include(__DIR__ . '/../../includes/db_connect.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
     $email = filter_var($_POST['email'] ?? '', FILTER_SANITIZE_EMAIL);
 
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // Insert email into database
-        $stmt = $conn->prepare("INSERT INTO newsletter_subscribers (email) VALUES (?)");
+
+        $stmt = $conn->prepare("SELECT email from newsletter_subscribers WHERE email =?");
         $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-        if ($stmt->execute()) {
-            echo json_encode(['success' => true, 'message' => 'Subscribed successfully']);
+        if ($result->num_rows > 0) {
+            echo json_encode(['success' => false, 'message' => 'This Email is already Subscribed!']);
         } else {
-            $_SESSION['status'] = 'Subscription failed. Please try again.';
-            echo json_encode(['success' => false, 'message' => 'Database error']);
-        }
+            $stmt = $conn->prepare("INSERT INTO newsletter_subscribers (email) VALUES (?)");
+            $stmt->bind_param("s", $email);
 
-        $stmt->close();
+            if ($stmt->execute()) {
+                echo json_encode(['success' => true, 'message' => 'Subscribed successfully']);
+            } else {
+                $_SESSION['status'] = 'Subscription failed. Please try again.';
+                echo json_encode(['success' => false, 'message' => 'Database error']);
+            }
+
+            $stmt->close();
+        }
     } else {
         echo json_encode(['success' => false, 'message' => 'Invalid email']);
     }
