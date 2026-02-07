1311b84ec5c70b2683367a74b699c6dfcc0f2753
GeloQTP <geloadrian24@gmail.com>
2026-02-07 16:41:00 +0800

    style: add sidebar to admin dashboard

--- public/admin/Admin Dashboard.php
+++ public/admin/Admin Dashboard.php
@@ -15,11 +15,225 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.lineicons.com/5.1/line/lineicons.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+
+    ::after,
+    ::before {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    li {
+        list-style: none;
+    }
+
+    h1 {
+        font-weight: 600;
+        font-size: 1.5rem;
+    }
+
+    body {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    .wrapper {
+        display: flex;
+    }
+
+    .main {
+        min-height: 100vh;
+        width: 100%;
+        overflow: hidden;
+        transition: all 0.35s ease-in-out;
+        background-color: #fafbfe;
+    }
+
+    #sidebar {
+        width: 70px;
+        min-width: 70px;
+        z-index: 1000;
+        transition: all .25s ease-in-out;
+        display: flex;
+        flex-direction: column;
+        background-color: #198754;
+    }
+
+    #sidebar.expand {
+        width: 260px;
+        min-width: 260px;
+    }
+
+    #toggle-btn {
+        background-color: transparent;
+        cursor: pointer;
+        border: 0;
+        padding: 1rem 1.5rem;
+    }
+
+    #toggle-btn i {
+        font-size: 1.5rem;
+        color: #fff;
+    }
+
+    .sidebar-logo {
+        margin: auto 0;
+    }
+
+    .sidebar-logo a {
+        color: #FFF;
+        font-size: 1.15rem;
+        font-weight: 600;
+    }
+
+    #sidebar:not(.expand) .sidebar-logo,
+    #sidebar:not(.expand) a.sidebar-link span {
+        display: none;
+    }
+
+    .sidebar-nav {
+        padding: 2rem 0;
+        /* flex: 1 1 auto; */
+    }
+
+    a.sidebar-link {
+        padding: .625rem 1.625rem;
+        color: #FFF;
+        display: block;
+        font-size: 0.9rem;
+        white-space: nowrap;
+        border-left: 3px solid transparent;
+    }
+
+    .sidebar-link i {
+        font-size: 1.1rem;
+        margin-right: .75rem;
+    }
+
+
+    a.sidebar-link:hover {
+        background-color: rgba(255, 255, 255, .075);
+        border-left: 3px solid #115c39ff;
+    }
+</style>
+
 <body>
-    
+
+    <div class="wrapper">
+        <aside id="sidebar">
+            <div class="d-flex">
+                <button id="toggle-btn">
+                    <i class="bi bi-grid"></i>
+                </button>
+
+                <div class="sidebar-logo">
+                    <a href="#">ADMIN</a>
+                </div>
+            </div>
+
+            <ul class="sidebar-nav">
+
+                <li class="navbar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-speedometer2"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="navbar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-people"></i>
+                        <span>User Management</span>
+                    </a>
+                </li>
+
+                <li class="navbar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-file-earmark-text"></i>
+                        <span>Content & Posts</span>
+                    </a>
+                </li>
+
+                <li class="navbar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-book"></i>
+                        <span>Academic Data</span>
+                    </a>
+                </li>
+
+                <li class="navbar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-award"></i>
+                        <span>Alumni Management</span>
+                    </a>
+                </li>
+
+                <li class="navbar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-envelope"></i>
+                        <span>Communications</span>
+                    </a>
+                </li>
+
+                <li class="navbar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-graph-up"></i>
+                        <span>Reports and Analytics</span>
+                    </a>
+                </li>
+
+                <li class="navbar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-gear"></i>
+                        <span>System Settings</span>
+                    </a>
+                </li>
+
+                <li class="navbar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-shield-check"></i>
+                        <span>Security and Audit</span>
+                    </a>
+                </li>
+
+            </ul>
+
+            <div class="sidebar-footer">
+                <a href="#" class="sidebar-link">
+                    <i class="bi bi-box-arrow-left"></i>
+                    <span>Logout</span>
+                </a>
+            </div>
+
+        </aside>
+
+        <div class="main p-3">
+            <div class="text-center">
+                <h1>Sidebar Bootstrap 5</h1>
+            </div>
+        </div>
+
+    </div>
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+
+<script>
+    const hamburger = document.querySelector("#toggle-btn");
+
+    hamburger.addEventListener("click", function() {
+        document.querySelector("#sidebar").classList.toggle("expand");
+    });
+</script>
 
 </html>
