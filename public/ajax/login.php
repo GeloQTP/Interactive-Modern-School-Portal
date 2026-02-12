80cd25648427dbbb43cf21095524e1e7b25f4cce
GeloQTP <geloadrian24@gmail.com>
2026-02-13 01:35:00 +0800

    refactor: change the name of the called table from the database in registerStudent.php

--- public/ajax/login.php
+++ public/ajax/login.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
     $passcode = $_POST['password'] ?? '';
 
-    $stmt = $conn->prepare("SELECT student_information.Email, accounts.account_username, accounts.account_password, accounts.role FROM accounts INNER JOIN student_information ON accounts.student_id = student_information.student_id WHERE student_information.Email = 'geloadrian24@gmail.com';");
+    $stmt = $conn->prepare("SELECT student_information.Email, users.account_username, users.account_password, users.role FROM users INNER JOIN student_information ON users.student_id = student_information.student_id WHERE student_information.Email = 'geloadrian24@gmail.com';");
 
     $stmt->bind_param("s", $email);
     $stmt->execute();
