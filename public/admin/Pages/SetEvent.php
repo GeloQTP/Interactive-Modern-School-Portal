1614ea1fe3272542be712a358fca06af83aa5e7f
GeloQTP <geloadrian24@gmail.com>
2026-03-08 01:11:53 +0800

    (ADMIN DASHBOARD) style: `Set Event Page` UI Improvements

--- public/admin/Pages/SetEvent.php
+++ public/admin/Pages/SetEvent.php
@@ -4,11 +4,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Set Event - Tomas Del Rosario College</title>
+    <link rel="icon" type="image/png" href="/Modern Student Portal/src/img/TRC_LOGO.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./../components/sidebar.css">
 </head>
 
 <body>
-    <iframe src="https://calendar.google.com/calendar/embed?src=geloadrian24%40gmail.com&ctz=Asia%2FManila" style="border: 0" width="1000" height="900" frameborder="0" scrolling="no"></iframe> // TODO: LEARN MORE ABOUT GOOGLE CALENDAR AND FIX THIS
+
+    <div class="wrapper">
+
+        <?php
+        include __DIR__ . '/../components/sidebar.php';
+        ?>
+
+        <div class="main ms-5 ps-4">
+
+            <nav class="navbar navbar-expand-lg bg-light border border-bottom">
+                <div class="container-fluid justify-content-center" style="transform: translate(0px, 10px);">
+                    <p class="text-success lead">Teacher Management</p>
+                </div>
+            </nav>
+
+            <iframe src="https://calendar.google.com/calendar/embed?src=geloadrian24%40gmail.com&ctz=Asia%2FManila" style="border: 0" width="1000" height="900" frameborder="0" scrolling="no"></iframe> // TODO: LEARN MORE ABOUT GOOGLE CALENDAR AND FIX THIS
+
+        </div>
+
+    </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+<script src="../scripts/sidebar.js"></script>
 
 </html>
